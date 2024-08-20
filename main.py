b2af25b6640d602e5db475280bf0adc6d6476846
vaproh <vaproh@proton.me>
2024-08-20 17:59:20 +0530

    added filename arguement and query group

--- main.py
+++ main.py
@@ -18,19 +18,40 @@ if __name__ == "__main__":
 # parser
 parser = argparse.ArgumentParser()
 
-parser.add_argument("-np", "--nowPlaying",
-                    help="Prints currently playing song name.",
-                    action="store_true")
+# arguements
+group = parser.add_argument_group("Query", "Query arguements for mpd.")
 
+# nowPlaying argument
+group.add_argument("-np", "--nowPlaying",
+                   help="Prints currently playing song name.",
+                   action="store_true")
+
+# fileName argument
+group.add_argument("-fn", "--filename",
+                   help="Prints the file name of the song currently playing.",
+                   action="store_true")
+
+# argument logic
 args = parser.parse_args()
 
+# nowPlaying arguement
 if args.nowPlaying:
-    songDict = client.currentsong()
+    songDict_NP = client.currentsong()
 
-    if songDict.get("title") is None and songDict.get("artist") is None:
-        print("Nothings being played currently.")
+    if songDict_NP.get("title") is None and songDict_NP.get("artist") is None:
+        print("Nothings being playing currently.")
     else:
-        print(f"{songDict.get("title")} by {songDict.get("artist")}")
+        print(f"{songDict_NP.get("title")} by {songDict_NP.get("artist")}")
+
+# fileName argument
+if args.filename:
+    songDict_FN = client.currentsong()
+
+    if songDict_FN.get("file") is None:
+        print("Nothings being playing currently.")
+    else:
+        print(f"Filename is \'{songDict_FN.get("file")}\'")
+
 
 # close and disconnect from the server
 client.close()
